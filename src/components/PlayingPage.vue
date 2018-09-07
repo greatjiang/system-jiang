a16852456a4f444d2f2fe1d39f8335824cfa4faf
greatjiang <1014512703.qq.com>
2018-09-07 09:07:09 +0800

    播放页布局

--- src/components/PlayingPage.vue
+++ src/components/PlayingPage.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <go-back @backaction="back"></go-back>
+    <div class="song-pictute-wrapper rotate">
+      <img src="../../static/princess.jpeg" alt="">
+    </div>
+    <div class="opreate-wrapper">
+      <div class="opreate-like"></div>
+      <div class="opreate-download"></div>
+      <div class="opreate-mes"></div>
+      <div class="opreate-more"></div>
+    </div>
     <div class="playing-wrapper">
       <audio src="##" id="bottleAudio"></audio>
       <div class="play-controller">
@@ -161,6 +170,109 @@ export default {
 }
 </script>
 <style scoped>
+.opreate-wrapper {
+  position: absolute;
+  bottom: 250px;
+  width: 10rem;
+  height: 100px;
+  line-height: 100px;
+  font-size: 0;
+  z-index: 200;
+}
+.opreate-like{
+  width: 64px;
+  height: 64px;
+  line-height: 64px;
+  background: url('../../static/like.png') no-repeat;
+  background-size: 100%;
+  display: inline-block;
+  vertical-align: middle;
+  margin-left: 100px;
+}
+.opreate-download{
+  width: 64px;
+  height: 64px;
+  line-height: 64px;
+  background: url('../../static/download.png') no-repeat;
+  background-size: 100%;
+  display: inline-block;
+  vertical-align: middle;
+  margin-left: 100px;
+}
+.opreate-mes{
+  width: 64px;
+  height: 64px;
+  line-height: 64px;
+  background: url('../../static/mes.png') no-repeat;
+  background-size: 100%;
+  display: inline-block;
+  vertical-align: middle;
+  margin-left: 110px;
+}
+.opreate-more{
+  width: 64px;
+  height: 64px;
+  line-height: 64px;
+  background: url('../../static/more.png') no-repeat;
+  background-size: 100%;
+  display: inline-block;
+  vertical-align: middle;
+  margin-left: 123px;
+}
+.song-pictute-wrapper {
+  position: absolute;
+  top: 350px;
+  left: 50%;
+  margin-left: -150px;
+  width: 300px;
+  height: 300px;
+  background: #000;
+  border-radius: 50%;
+  z-index: 200;
+  box-shadow: 0 0 0 8px rgb(41, 38, 38, .8),
+              0 0 0 12px rgba(255, 255, 255, 0.1),
+              0 0 0 16px rgba(0, 0, 0, 0.6),
+              0 0 0 20px rgba(255, 255, 255, 0.1),
+              0 0 0 24px rgba(0, 0, 0, 0.6),
+              0 0 0 28px rgba(255, 255, 255, 0.1),
+              0 0 0 32px rgba(0, 0, 0, 0.6),
+              0 0 0 36px rgba(255, 255, 255, 0.1),
+              0 0 0 40px rgba(0, 0, 0, 0.6),
+              0 0 0 44px rgba(255, 255, 255, 0.1),
+              0 0 0 46px rgba(0, 0, 0, 0.6),
+              0 0 0 50px rgba(255, 255, 255, 0.1),
+              0 0 0 54px rgba(0, 0, 0, 0.6),
+              0 0 0 58px rgba(255, 255, 255, 0.1),
+              0 0 0 62px rgba(0, 0, 0, 0.6),
+              0 0 0 66px rgba(255, 255, 255, 0.1),
+              0 0 0 70px rgba(0, 0, 0, 0.6),
+              0 0 0 74px rgba(255, 255, 255, 0.1),
+              0 0 0 78px rgba(0, 0, 0, 0.6),
+              0 0 0 82px rgba(255, 255, 255, 0.1),
+              0 0 0 86px rgba(0, 0, 0, 0.6),
+              0 0 0 90px rgba(255, 255, 255, 0.1),
+              0 0 0 94px rgba(0, 0, 0, 0.6),
+              0 0 0 98px rgba(255, 255, 255, 0.1),
+              0 0 0 102px rgba(0, 0, 0, 0.6),
+              0 0 0 106px rgba(255, 255, 255, 0.1),
+              0 0 0 115px rgba(214, 213, 213, .6);
+}
+@keyframes rotateAction {
+  form {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+.song-pictute-wrapper.rotate {
+  animation: rotateAction 12s linear infinite;
+}
+.song-pictute-wrapper>img {
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+}
 .playing-wrapper {
   position: fixed;
   padding-top: 100px;
@@ -168,15 +280,15 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgb(31, 30, 30);
+  background: rgb(243, 241, 241);
   z-index: 100;
 }
 .play-controller {
   position: absolute;
   bottom: 0;
   width: 10rem;
-  height: 150px;
-  background-color: #eee;
+  height: 200px;
+  /* background-color: #eee; */
 }
 .progress-wrapper {
   width: 10rem;
@@ -207,6 +319,7 @@ export default {
   line-height: 100px;
   font-size: 0;
   margin: 0 auto;
+  margin-top: 20px;
 }
 .play-prev {
   width: 64px;
